fcb284a281275e5f6af8b1879710e2716c8513ad
Amirhossein <amirhossein693@gmail.com>
2026-04-26 17:08:17 +0330

    Revamp resume with English modern high-performance layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,3 @@
-(function($){
-
- 	$(".js-lazy").lazyload({
- 		effect : "fadeIn"
- 	});
-
- 	$('.js-modal').modal();
-
-	console.log("Hi, I'm Frontend Developer");
-})(jQuery);
+(function () {
+  document.documentElement.classList.add('js-ready');
+})();
